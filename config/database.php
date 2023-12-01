8930ad4892e1f4b984ff406b0f49e0187ac4007b
danielwidhiarto <danielwidhiarto@gmail.com>
2023-12-01 16:46:04 +0700

    test2 db

--- config/database.php
+++ config/database.php
@@ -66,7 +66,8 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST','POSTGRES_HOST', '127.0.0.1'),
+            // 'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
