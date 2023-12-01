ad2d2683959f710046b1c3643ec03dddd6c1d7fb
danielwidhiarto <danielwidhiarto@gmail.com>
2023-12-01 16:56:13 +0700

    fix password login

--- config/database.php
+++ config/database.php
@@ -70,7 +70,7 @@ return [
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
